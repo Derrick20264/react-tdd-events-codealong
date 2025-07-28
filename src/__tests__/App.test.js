065ef3f795f5821d3d5d5cd0c20f7f8eccce0189
derrick <derrick.mwangi2@student.moringaschool.com>
2025-07-29 02:29:48 +0300

    Build pizza topping selector with TDD and user interactions

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import App from "../App";
 
-import '@testing-library/jest-dom';
-
-// Code tests here
+test("displays a top-level heading with the text `Hi, I'm Derrick`", () => {
+  render(<App />);
+  const topHeading = screen.getByRole("heading", {
+    name: /hi, i'm derrick/i,
+    level: 1
+  });
+  expect(topHeading).toBeInTheDocument();
+});
